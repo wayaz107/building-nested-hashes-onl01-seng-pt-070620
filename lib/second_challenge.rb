d5522aa66da4457f6285ad5e77bd5a681ab57ce9
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 07:16:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => []
-      },
-    :capulet => {
-      #your key/value pairs here!
-      :patriarch => {}
-:matriarch => {}
-:heroine => {}
-:heroine_friends => {}
-      }
-    }
-
+     :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+     }
+  }
 
 end
