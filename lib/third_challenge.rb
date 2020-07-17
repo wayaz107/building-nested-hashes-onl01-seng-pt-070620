8030e9b0157fabdd3256fe8d7390aedc9f38d605
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 07:19:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,16 +2,24 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        #your key/value pairs here :name of "Lord Montague" and
+ :name => "Lord Montague"
+ :age => "53"
+
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
+        :name => "Romeo"
+        :age =>"15"
+        :status =>"alive".
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +34,5 @@ def third_challenge
    }
   }
 
-  
+
 end
